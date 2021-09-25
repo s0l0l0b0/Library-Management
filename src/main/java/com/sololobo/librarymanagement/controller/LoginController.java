2fe6a1b0d64a28750d157993c4c277f4507f76a5
sololobo <smshaon02@yahoo.com>
2021-09-25 11:24:21 +0800

    created utility methods to distinguish logged-in user and admin user

--- src/main/java/com/sololobo/librarymanagement/controller/LoginController.java
+++ src/main/java/com/sololobo/librarymanagement/controller/LoginController.java
@@ -1,16 +1,20 @@
 package com.sololobo.librarymanagement.controller;
 
+import com.sololobo.librarymanagement.utility.Utility;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login(){
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        if (Objects.nonNull(authentication)) {
-//            return "redirect:/";
-//        }
+    public String login() {
+        if (Utility.isAdminUser()) {
+            return "admin";
+        }
+        if (Utility.isLoggedInUser()) {
+            return "redirect:/";
+        }
         return "login";
     }
 }
--- src/main/java/com/sololobo/librarymanagement/utility/Utility.java
+++ src/main/java/com/sololobo/librarymanagement/utility/Utility.java
@@ -1,6 +1,11 @@
 package com.sololobo.librarymanagement.utility;
 
 
+import com.sololobo.librarymanagement.domain.enumeration.Role;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import java.util.Objects;
 
 public class Utility {
@@ -11,5 +16,15 @@ public class Utility {
         }
         String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
         return email.matches(regex);
+    }
+
+    public static boolean isLoggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Objects.nonNull(authentication) && !authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
+    }
+
+    public static boolean isAdminUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Objects.nonNull(authentication) && authentication.getAuthorities().contains(new SimpleGrantedAuthority(Role.ADMIN.name()));
     }
 }
